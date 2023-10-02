de94560533f6ac44be16761a7b4785607b14805d
Antonela <antonelaborgogno@gmail.com>
2023-10-02 11:20:50 -0300

    inicio de repo

---
+++ src/components/Api.jsx
@@ -0,0 +1,19 @@
+import { useState, useEffect } from 'react';
+
+const Api = () => {
+
+    const [users,setUsers] = useState([]);
+
+    useEffect(( ) => {},)
+    return (
+        <div>
+            <h2>Ejemplo de Api</h2>
+            <ul>
+
+            </ul>
+
+        </div>
+    )
+}   
+
+export default Api   
